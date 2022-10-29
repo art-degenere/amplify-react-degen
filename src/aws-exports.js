c6111a6852198a034577e1666e9736262f179719
arobillard-ledger <alexandre.robillard-ext@ledger.fr>
2022-10-29 11:58:46 +0200

    database + notes system

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://6ixonjghsbc2roanmruzaistsm.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-hxh3zgzry5ct7f6mxphsrgevxe"
 };
 
 
