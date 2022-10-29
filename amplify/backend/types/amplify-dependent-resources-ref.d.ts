ed2ad1f8834170de2c7a1c56dab7bdbe2b1d2957
arobillard-ledger <alexandre.robillard-ext@ledger.fr>
2022-10-29 09:22:52 +0200

    NOW WITH REAL BITES OF AUTH INSIDE + downgrade aws-amplify/ui-react

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactdegenaf377b01": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
